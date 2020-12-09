f0da3da99016efeb8459cb25f560475915a26f84
wangsongwen <2544894086wsw@gmail.com>
2020-12-09 09:39:31 +0800

    2020-12-9 update

---
+++ src/main/java/com/wsw/redislock/entity/RedisLock.java
@@ -0,0 +1,9 @@
+package com.wsw.redislock.entity;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 9:37 2020/12/9
+ * @Description:
+ */
+public class RedisLock {
+}
--- src/test/java/com/wsw/redislock/RedislockApplicationTests.java
+++ src/test/java/com/wsw/redislock/RedislockApplicationTests.java
@@ -1,13 +1,21 @@
 package com.wsw.redislock;
 
+import com.wsw.redislock.service.RedisLockService;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RedislockApplicationTests {
+    @Autowired
+    private RedisLockService redisLockService;
+    @Autowired
+    private RedissonClient redissonClient;
 
     @Test
     void contextLoads() {
+        redisLockService.testUnLock();
     }
 
 }
